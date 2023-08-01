fcfac64ed95f67847a16805a95ff4db8d3eb58b6
muhamadfajarID <141119287+muhamadfajarID@users.noreply.github.com>
2023-08-01 14:16:23 +0700

    Add files via upload
    Tugas WorkShop Nusa Putra

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(543, 128)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 26)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Insert Image"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(543, 164)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(120, 26)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Convert"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.Control
+        Me.Button3.Location = New System.Drawing.Point(543, 197)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Button3.Size = New System.Drawing.Size(120, 26)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(543, 231)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(73, 19)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Strecth"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Enabled = False
+        Me.Label1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Label1.Location = New System.Drawing.Point(5, 277)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 15)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Created By"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Enabled = False
+        Me.Label2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Label2.Location = New System.Drawing.Point(96, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Source"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Enabled = False
+        Me.Label3.ImageAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Label3.Location = New System.Drawing.Point(363, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 15)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Result"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.WindowsApplication1.My.Resources.Resources.download_removebg_preview
+        Me.PictureBox3.Location = New System.Drawing.Point(562, 33)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(82, 81)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 9
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.Location = New System.Drawing.Point(262, 33)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(254, 226)
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(8, 33)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(246, 226)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkVisited = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(78, 277)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(105, 15)
+        Me.LinkLabel1.TabIndex = 10
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Muhamad Fajar"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(676, 301)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("News701 BT", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Build Apps Muhamad Fajar"
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,151 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
+            Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim r, g, b As Integer
+        Dim thresh As Integer
+        thresh = 100
+
+        Dim gray As Integer
+        Dim picSource As New Bitmap(PictureBox1.Image)
+        Dim picDest = picSource
+        PictureBox2.Image = picDest
+
+        For i = 1 To picSource.Width - 1
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                gray = CInt((r + g + b) / 3)
+
+                If gray >= thresh Then
+                    r = 184
+                    g = 220
+                    b = 244
+                Else
+                    r = 0
+                    g = 0
+                    b = 0
+                End If
+                picDest.SetPixel(i, j, Color.FromArgb(r, g, b))
+
+            Next
+        Next
+        For i = 40 To picSource.Width - 180
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                gray = CInt((r + g + b) / 3)
+
+                If gray >= thresh Then
+                    r = 232
+                    g = 168
+                    b = 17
+                Else
+                    r = 0
+                    g = 0
+                    b = 0
+                End If
+                picDest.SetPixel(i, j, Color.FromArgb(r, g, b))
+
+            Next
+        Next
+        For i = 50 To picSource.Width - 90
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                gray = CInt((r + g + b) / 3)
+
+                If gray >= thresh Then
+                    r = 104
+                    g = 202
+                    b = 133
+                Else
+                    r = 0
+                    g = 0
+                    b = 0
+                End If
+                picDest.SetPixel(i, j, Color.FromArgb(r, g, b))
+
+            Next
+        Next
+        For i = 80 To picSource.Width - 90
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                gray = CInt((r + g + b) / 3)
+
+                If gray >= thresh Then
+                    r = 237
+                    g = 94
+                    b = 61
+                Else
+                    r = 0
+                    g = 0
+                    b = 0
+                End If
+                picDest.SetPixel(i, j, Color.FromArgb(r, g, b))
+
+            Next
+        Next
+        For i = 140 To picSource.Width - 50
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                gray = CInt((r + g + b) / 3)
+
+                If gray >= thresh Then
+                    r = 232
+                    g = 168
+                    b = 17
+                Else
+                    r = 0
+                    g = 0
+                    b = 0
+                End If
+                picDest.SetPixel(i, j, Color.FromArgb(r, g, b))
+
+            Next
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim keluar As String
+        keluar = MsgBox("Are You Sure ?", vbQuestion + vbYesNo, "Confirm")
+        If keluar = vbYes Then
+            Me.Close()
+        ElseIf keluar = vbNo Then
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If (CheckBox1.Checked) Then
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+            PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        Else
+            PictureBox1.SizeMode = PictureBoxSizeMode.Normal
+            PictureBox2.SizeMode = PictureBoxSizeMode.Normal
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        LinkLabel1.LinkVisited = True
+        System.Diagnostics.Process.Start("https://www.facebook.com/fajar.dbrantakan/")
+    End Sub
+End Class
